357a1d3ce4784f6a97b3bec600594f56dcee454b
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-11-29 20:41:21 -0500

    Create Withdata4.sh

---
+++ Withdata4.sh
@@ -0,0 +1,2 @@
+#This script is for ZL 5 or larger than 5.
+for ((z=5; z<7; z++)); do mkdir ${z}; echo ${z}; date; for((n=0; n<2**(z-5); n++)); do mkdir ${z}/$((n*32)) ; for((m=0; m<2**(z-5); m++)); do mkdir ${z}/$((n*32))/$((m*32)); mkdir ${z}/$((n*32))/$((m*32))/32; mkdir ${z}/$((n*32))/$((m*32))/32/32; cp src/index00.json ${z}/$((n*32))/$((m*32))/32/32/index.json; n1=$((n*32)); m1=$((m*32)) ;echo \"location\":\{\"left\":${n1},\"top\":${m1},\"width\":32,\"height\":32\}\} >> ${z}/$((n*32))/$((m*32))/32/32/index.json; done; done; echo ${z} ok; date; done;
